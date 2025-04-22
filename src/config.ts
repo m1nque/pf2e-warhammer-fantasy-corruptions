d74323fc6bac445355ed02657ff7392905f93686
김민규 <min_q@donga.com>
2025-04-22 18:33:21 +0900

    Scene 훅 추가

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const chaosGods = ["khorne", "nurgle", "tzeentch", "slaanesh"];
+
+export default {
+  MODULE_ID: "pf2e-warhammer-fantasy-corruptions",
+  MODULE_NAME: "Chaos Corruption",
+  chaosGods
+};
--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,5 @@
 import '@illandril/foundryvtt-types';
-
-// 모듈 메타정보
-export const MODULE_ID = "pf2e-warhammer-fantasy-corruptions";
-export const MODULE_NAME = "Chaos Corruption";
-
+import config from './config';
 
 /**
  * 로컬라이즈 헬퍼
@@ -11,5 +7,5 @@ export const MODULE_NAME = "Chaos Corruption";
  * @returns 로컬라이즈된 문자열
  */
 export function localize(key: string): string {
-  return game.i18n.localize(`${MODULE_ID}.${key}`);
+  return game.i18n.localize(`${config.MODULE_ID}.${key}`);
 }
