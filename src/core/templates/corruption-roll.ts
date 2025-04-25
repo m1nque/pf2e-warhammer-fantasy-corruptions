69586758531818a0195060947f36da84090eb6e6
김민규 <min_q@donga.com>
2025-04-25 13:22:47 +0900

    modified:   src/core/templates/corruption-roll.ts

--- src/core/templates/corruption-roll.ts
+++ src/core/templates/corruption-roll.ts
@@ -3,17 +3,19 @@
 export interface CorruptionRoll {
   content: string;
   type: 'reflex' | 'will' | 'fortitude';
-  dc: number;
+  dc?: number;
   proficiency: number;  
 }
 
 export function appendCorruptionSaveButton(roll: CorruptionRoll) {
 
+  const action = `corruption${roll.type}-save`;
+
   const corruptionIcon = ``;
 
     const willSave = `
-    <div class="chaos-conditions-will-save">
-      <button type="button" data-action="will-save">${corruptionIcon} Will Save</button>
+    <div class="chaos-conditions-${action}">
+      <button type="button" data-action="${action}">${corruptionIcon} Will Save</button>
     </div>
   `;
 
